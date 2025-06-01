eafa7a69ba6339720d39ccb3f60d3aafd5ce53d8
Daniel Nunes <danyribeironunes15@gmail.com>
2025-06-01 11:24:47 +0100

    refactor: Add base route to work with nginx proxy

--- index.ts
+++ index.ts
@@ -2,15 +2,16 @@ import Fastify from "fastify";
 import fyWebSocket from "@fastify/websocket";
 import services from "./src/services";
 
+const BASE_ROUTE = "/api/v1";
 const fastify = Fastify();
 
 fastify.register(fyWebSocket);
 
 fastify.register(async fastify => {
-    fastify.get("/transfer/create", { websocket: true }, services.createTransfer);
-    fastify.get("/transfer/:code/check", services.checkTransfer);
-    fastify.get("/transfer/:code", { websocket: true }, services.answerTransfer);
-    fastify.get("/credentials", services.getCredentials);
+    fastify.get(BASE_ROUTE + "/transfer/create", { websocket: true }, services.createTransfer);
+    fastify.get(BASE_ROUTE + "/transfer/:code/check", services.checkTransfer);
+    fastify.get(BASE_ROUTE + "/transfer/:code", { websocket: true }, services.answerTransfer);
+    fastify.get(BASE_ROUTE + "/credentials", services.getCredentials);
 });
 
 fastify.listen({ port: 8020, host: "0.0.0.0" }, err => {
