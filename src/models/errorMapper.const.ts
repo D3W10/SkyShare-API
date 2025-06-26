21eb9c25a4be1d21056ba23ae9119158bf4eeda6
Daniel Nunes <danyribeironunes15@gmail.com>
2025-06-26 20:22:17 +0100

    refactor: Change refresh token error type

--- src/models/errorMapper.const.ts
+++ src/models/errorMapper.const.ts
@@ -11,6 +11,6 @@ export const errorMapper: Record<ErrorList, ErrorCause> = {
     miscommunication: "userError",
     stateMismatch: "forbiddenError",
     authenticationFailed: "userError",
-    unableToRefreshToken: "serverError",
+    unableToRefreshToken: "userError",
     unknown: "serverError"
 };
